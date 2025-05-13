da5d4993510e759a475a7ba2632c43036bd60df9
Jaxon <jaxonlloyd05@gmail.com>
2025-05-13 18:28:16 +1000

    motor inclusion

--- src/motor_controller.py
+++ src/motor_controller.py
@@ -4,27 +4,28 @@ from gpiozero.pins.rpigpio import RPiGPIOFactory
 class DRCMotorController:
     def __init__(self, motorPin, servoPin):
         # GPIO13 & GPIO12 preferable
-        # self.motor = PWMOutputDevice(pin=motorPin, frequency=100)
+        self.motor = PWMOutputDevice(pin=motorPin, frequency=100)
         Device.pin_factory = RPiGPIOFactory()
         self.servo = Servo(servoPin)  
         pass
 
     def setDrivingMotor(self, speed):
         # speed taken in between 0 and 1 - still to decide how to do this
+        self.motor.value = speed
         pass
 
     def setServoMotor(self, angle):
-        # angle from 0 to 1 with gpiozero
+        # angle from -1 to 1 for servo class
         self.servo.value = angle
         pass
 
     def off (self):
-        # self.motor.off()
+        self.motor.off()
         self.servo.off()
         pass
     
     def on (self):
-        # self.motor.on()
+        self.motor.on()
         self.servo.on()
         pass
 
